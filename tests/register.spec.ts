5571abfd07578127d3ce8464288f2d8846551914
vascsx <vasczn@gmail.com>
2025-07-01 22:15:02 -0300

    refactor: archives test

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -4,13 +4,13 @@ import { RegisterPage } from '../pages/RegisterPage';
 
 const { email: VALID_EMAIL, fullName: FULL_NAME, password: VALID_PASSWORD, registerUrl: REGISTER_URL } = registerData;
 
-test.only('Should register successfully using Admin profile', async ({ page }) => {
+test('Should register successfully using Admin profile', async ({ page }) => {
   const registerPage = new RegisterPage();
   await registerPage.register(page, REGISTER_URL, VALID_EMAIL, FULL_NAME, VALID_PASSWORD, VALID_PASSWORD);
   await page.waitForSelector('h1:has-text("Dashboard")', { state: 'visible' });
 });
 
-test.only('Should is not register with already Email', async ({ page }) => {
+test('Should is not register with already Email', async ({ page }) => {
   const registerPage = new RegisterPage();
   const alreadyEmail = registerData.invalidEmail;
   await registerPage.register(page, REGISTER_URL, alreadyEmail, FULL_NAME, VALID_PASSWORD, VALID_PASSWORD);
