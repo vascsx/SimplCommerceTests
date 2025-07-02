b4c1e0207dd4973af631b46f7860391079e564ec
vascsx <vasczn@gmail.com>
2025-07-02 13:20:16 -0300

    feat: add random email logic

--- tests/register.spec.ts
+++ tests/register.spec.ts
@@ -1,21 +1,20 @@
 import { test, expect } from '@playwright/test';
+import { faker } from '@faker-js/faker'
 import registerData from '../utils/register.json';
 import { RegisterPage } from '../pages/RegisterPage';
 
-const { email: VALID_EMAIL, fullName: FULL_NAME, password: VALID_PASSWORD, registerUrl: REGISTER_URL } = registerData;
+const { fullName: FULL_NAME, password: VALID_PASSWORD, registerUrl: REGISTER_URL } = registerData;
 
 test('Should register successfully using Admin profile', async ({ page }) => {
   const registerPage = new RegisterPage();
+  const VALID_EMAIL = faker.internet.email();
   await registerPage.register(page, REGISTER_URL, VALID_EMAIL, FULL_NAME, VALID_PASSWORD, VALID_PASSWORD);
   await page.waitForSelector('h1:has-text("Dashboard")', { state: 'visible' });
 });
 
-test('Should is not register with already Email', async ({ page }) => {
+test('Should not register with already Email', async ({ page }) => {
   const registerPage = new RegisterPage();
   const alreadyEmail = registerData.invalidEmail;
   await registerPage.register(page, REGISTER_URL, alreadyEmail, FULL_NAME, VALID_PASSWORD, VALID_PASSWORD);
   await expect(page.locator(`li:has-text("Username '${alreadyEmail}' is already taken.")`)).toBeVisible();
 });
-
-
-
